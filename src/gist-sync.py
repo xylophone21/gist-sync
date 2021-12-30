f1f212285816470f9fd2cfc7fbbdbfccff0fbe9e
Hui.Li-TCL <xylophone21@gmail.com>
2021-12-30 18:18:45 +0800

    fix python cmd error in test.yaml

--- src/gist-sync.py
+++ src/gist-sync.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import click
+from click.types import STRING
 
 @click.group()
 @click.pass_context
@@ -7,16 +8,23 @@ def main(context):
     context.obj = {"debug": False}
 
 @main.command()
-@click.argument(
-    'root',
-    type=click.Path(file_okay=False, resolve_path=True))
-@click.argument(
-    'token')
+@click.option(
+    '--root',
+    default=".",
+    type=click.Path(exists=True, file_okay=False, resolve_path=True),
+    help='Root directory to sync.')
+@click.option(
+    '--user',
+    default=None,
+    type=STRING,
+    help='Default gist user.')
+@click.argument('token')
 @click.pass_context
-def build(context,root,token):
+def build(context,root,token,user):
     click.echo('build:' + str(context))
     click.echo('build:' + str(root))
     click.echo('build:' + str(token))
+    click.echo('build:' + str(user))
 
 if __name__ == '__main__':
     main()
